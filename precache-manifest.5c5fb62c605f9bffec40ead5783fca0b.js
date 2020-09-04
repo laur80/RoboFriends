00e276216cfd9413aae383ec2db9e8adbb1356fa
“Laur” <laur80raul@gmail.com>
2020-09-04 18:17:16 +0300

    Updates

---
+++ precache-manifest.5c5fb62c605f9bffec40ead5783fca0b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "466b6e8cecc067f62dee520ac69fb5df",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "0d23f12c7761eed3e420",
+    "url": "/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "0d23f12c7761eed3e420",
+    "url": "/robofriends/static/js/2.9b02e71a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.9b02e71a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d935aa80b751e012397",
+    "url": "/robofriends/static/js/main.3e2ff49e.chunk.js"
+  },
+  {
+    "revision": "e7d2cbb368e0813da61d",
+    "url": "/robofriends/static/js/runtime-main.74eee7b5.js"
+  }
+]);
